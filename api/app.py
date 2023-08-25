26d649a6e65891dcdf8489f3e3b921ce84db8f84
KO1231 <124903312+KO1231@users.noreply.github.com>
2023-08-25 13:56:26 +0900

    [add] simple callback code(test)

--- api/app.py
+++ api/app.py
@@ -1,12 +1,42 @@
-from flask import Flask
+from flask import Flask, request
+import openai
+import os
 
 app = Flask(__name__)
 
+openai.organization = os.getenv("env_openai_orgid")
+openai.api_key = os.getenv("env_openai_apikey")
+MODEL = "gpt-3.5-turbo"
+SYSTEM_PROMPT = "You are a helpful assistant. Please answer in Japanese."
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def run(content: str) -> str:
+    # call OpenAI's ChatCompletion.
+    cmpl = openai.ChatCompletion.create(
+        model=MODEL,
+        messages=[
+            {"role": "system", "content": SYSTEM_PROMPT},
+            {"role": "user", "content": content}
+        ],
+        temperature=0
+    )
+    return cmpl["choices"][0]["message"]["content"]
 
+@app.route('/', methods=["POST"])
+def callback_post():
+    # get user's prompt from frontend.
+    content = request.json["content"]
 
-if __name__ == '__main__':
-    app.run()
+    # call OpenAI API.
+    result = run(content)
+    return result
+
+@app.route('/', methods=["GET"])
+def callback_get():
+    # debug
+    return "THIS IS GET."
+
+@app.route('/test', methods=["POST"])
+def callback_post_test():
+    # debug
+    content = request.json["content"]
+    return content
